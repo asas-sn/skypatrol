6a5d16f1952820c4537dd7dc3569d684a3ab0701
gonzodeveloper <kylehart@hawaii.edu>
2020-09-23 14:57:40 -1000

    pyarrow version force

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='pyasassn',
-      version='0.4.5',
+      version='0.4.6',
       url="https://github.com/gonzodeveloper/bad_asas_sn/",
       author='Kyle Hart',
       author_email='kylehart@hawaii.edu',
       license='GPL v.3',
       packages=['pyasassn'],
-      install_requires=['requests', 'pymongo', 'pandas==1.0.5', 'pyarrow', 'astropy==4.0.1', 'numpy==1.16.0'],
+      install_requires=['requests', 'pymongo', 'pandas==1.0.5', 'pyarrow==0.17.1', 'astropy==4.0.1', 'numpy==1.16.0'],
       zip_safe=False)
