db57d6480269ef725ba4fabb1e98a7963a0eb7b1
gonzodeveloper <kylehart@hawaii.edu>
2021-07-07 16:39:06 -1000

    new versioning

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='pyasassn',
       author_email='kylehart@hawaii.edu',
       license='GPL v.3',
       packages=['pyasassn'],
-      install_requires=['requests', 'pymongo', 'pandas==1.0.5', 'pyarrow==0.17.1', 'astropy==4.0.1', 'numpy==1.16.0'],
+      install_requires=['requests', 'pymongo', 'pandas==1.0.5', 'pyarrow==4.0.1', 'astropy==4.0.1', 'numpy==1.19.0'],
       zip_safe=False)
