3eabed03a936a76c41652036de611431a3434ac4
Daniel Hey <danielhey@outlook.com>
2022-06-02 16:42:32 -1000

    some QoL to setup

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
-from setuptools import setup
+#!/usr/bin/env python
+
+import os
+import sys
+from setuptools import setup, Extension
+
+# Prepare and send a new release to PyPI
+# if "release" in sys.argv[-1]:
+#     os.system("python setup.py sdist bdist_wheel")
+#     os.system("twine upload dist/*")
+#     os.system("rm -rf dist/skypatrol*")
+#     sys.exit()
+
+# Load the __version__ variable without importing the package already
+# exec(open("skypatrol/version.py").read())
+
+# Get dependencies
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
 
 setup(name='pyasassn',
       version='0.6.4',
@@ -7,10 +25,5 @@ setup(name='pyasassn',
       author_email='kylehart@hawaii.edu',
       license='GPL v.3',
       packages=['pyasassn'],
-      install_requires=[
-            'requests',
-            'pandas',
-            'pyarrow==4.0.1',
-            'astropy',
-            'numpy'],
+      install_requires=install_requires,
       zip_safe=False)
