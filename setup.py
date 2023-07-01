aeae84e34196c1fdf17147b6e7e25077eb39c18d
wintergreen <doctor.kyle.hart@gmail.com>
2023-06-30 18:34:19 -1000

    new version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="skypatrol",
-    version="0.6.11",
+    version="0.6.12",
     url="https://github.com/asas_sn/skypatrol/",
     author="ASAS-SN",
     author_email="kylehart@hawaii.edu",
